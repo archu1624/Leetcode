ea62c287d3ffcbcd7399a450988923e326cd9b66
archu1624 <archu16242006@gmail.com>
2026-08-13 14:10:38 +0530

    Time: 6 ms (3.43%) | Memory: 46.8 MB (24.84%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        int i = 0;
+
+        for (int num : set) {
+            nums[i] = num;
+            i++;
+        }
+
+        return i;
+    }
+}
